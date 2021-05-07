000d0d4f6d6d537b0b657e93ba7852ecbc7787c5
JohnMoran <johnnymoran44@hotmail.com>
2021-05-07 14:14:49 +0200

    Update material.module.ts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,15 +4,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
-import { MatSortModule } from '@angular/material/sort';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 
@@ -24,12 +21,9 @@ const MaterialComponents = [
   MatFormFieldModule,
   MatInputModule,
   MatPaginatorModule,
-  MatSortModule,
-  MatChipsModule,
   MatBadgeModule,
   MatIconModule,
   MatDialogModule,
-  MatSnackBarModule,
   MatSelectModule,
   MatRadioModule,
 ];
