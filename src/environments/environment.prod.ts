d5199c3194ed36590fec8fa60c35406c81507ea0
JohnMoran <johnnymoran44@hotmail.com>
2021-05-10 13:23:46 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://frontend-tech-test-data.s3.eu-west-1.amazonaws.com',
+  API_URL:
+    'https://frontend-tech-test-data.s3.eu-west-1.amazonaws.com/items.json',
 };
